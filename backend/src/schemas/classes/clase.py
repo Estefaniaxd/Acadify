b4401ba0a1c8c8b736b47c6c8d7e0c79cdc13ae7
HarrisonGuerrero18 <hguerreropalacios17@gmail.com>
2025-09-17 11:36:50 -0500

    Correciones crud

--- backend/src/schemas/classes/clase.py
+++ backend/src/schemas/classes/clase.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from uuid import UUID
+from uuid import UUID, uuid4
 from datetime import datetime, timedelta
 
 
@@ -11,6 +11,19 @@ class ClaseBase(BaseModel):
     hora_inicio: datetime
     duracion: timedelta
     link_videollamada: str
+    
+clase = ClaseBase(
+    grupo_curso_id=uuid4(),
+    plataforma_id=uuid4(),
+    titulo="Clase 1",
+    descripcion="Descripción de la clase",
+    hora_inicio=datetime(2025, 2, 2, 14, 24, 23),
+    duracion=timedelta(minutes=60),
+    link_videollamada="https://meet.google.com/abc-def-ghi"
+)
+
+clase_json_str = clase.model_dump_json()
+print(clase_json_str)
 
 
 class ClaseCreate(ClaseBase):
